796c2f57bac1ab75e01b80da74cb79d4f4b361bb
Angel André Cotoc Cuzal <78944937+AngelAndreCotocCuzal@users.noreply.github.com>
2022-03-24 23:48:00 -0600

    Subiendo todo

---
+++ views/prestamo_bancarios_ventana.py
@@ -0,0 +1,68 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from solicitar_ventana import Ui_Form_solicitar
+from imagenes import imagenes
+
+
+class Ui_MainWindow_prestamo(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(978, 717)
+        MainWindow.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        MainWindow.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(-20, -40, 1021, 731))
+        self.label.setStyleSheet("image: url(:/imagenes/fondo_proyecto.png);")
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(330, 70, 341, 61))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.btn_solicitar = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_solicitar.setGeometry(QtCore.QRect(180, 220, 141, 61))
+        self.btn_solicitar.setObjectName("btn_solicitar")
+        self.btn_planes = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_planes.setGeometry(QtCore.QRect(400, 220, 131, 61))
+        self.btn_planes.setObjectName("btn_planes")
+        self.btn_aprobar = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_aprobar.setGeometry(QtCore.QRect(610, 220, 141, 61))
+        self.btn_aprobar.setObjectName("btn_aprobar")
+        self.btn_visualizar = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_visualizar.setGeometry(QtCore.QRect(290, 330, 131, 61))
+        self.btn_visualizar.setObjectName("btn_visualizar")
+        self.btn_pagos = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_pagos.setGeometry(QtCore.QRect(500, 330, 131, 61))
+        self.btn_pagos.setObjectName("btn_pagos")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.btn_solicitar.clicked.connect(self.sol_view)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label_2.setText(_translate("MainWindow", "<html><head/><body><p><span style=\""
+                                                      " font-size:16pt; color:#ffffff;\""
+                                                      ">Prestamos Bancarios</span></p></body></html>"))
+        self.btn_solicitar.setText(_translate("MainWindow", "Solicitar"))
+        self.btn_planes.setText(_translate("MainWindow", "Planes"))
+        self.btn_aprobar.setText(_translate("MainWindow", "Aprobar"))
+        self.btn_visualizar.setText(_translate("MainWindow", "Visualizar"))
+        self.btn_pagos.setText(_translate("MainWindow", "Pagos"))
+
+    def sol_view(self):
+        self.ventana = QtWidgets.QMainWindow()
+        self.ui = Ui_Form_solicitar()
+        self.ui.setupUi(self.ventana)
+        self.ventana.show()
+
---
+++ views/solicitar_ventana.py
@@ -0,0 +1,160 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
+from imagenes import imagenes
+import json
+
+
+class Ui_Form_solicitar(object):
+    SCHEMA = {
+        'code_prestamo': {
+            'required': True,
+            'type': 'string',
+            'minlength': 1,
+            'maxlength': 8
+        },
+        'code_asociado': {
+            'required': True,
+            'type': 'string',
+            'minlength': 1,
+            'maxlength': 8
+        },
+        'monto_solicitado': {
+            'required': True,
+            'type': 'string',
+        },
+        'monto_aprobado': {
+            'type': 'string'
+        },
+        'numero_cuotas': {
+            'type': 'integer',
+            'minlength': 1,
+            'maxlength': 50
+        },
+        'ingreso_mensual': {
+            'required': True,
+            'type': 'string',
+        },
+        'garantia': {
+            'required': True,
+            'type': 'string'
+        },
+        'plan_pagos': {
+            'type': 'string'
+        },
+        'estado_prestamo': {
+            'type': 'string'
+        }
+    }
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(979, 657)
+        Form.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        Form.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(-60, -30, 1081, 691))
+        self.label.setStyleSheet("image: url(:/imagenes/fondo_proyecto.png);")
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(Form)
+        self.label_2.setGeometry(QtCore.QRect(100, 90, 261, 41))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.lnx_monto = QtWidgets.QLineEdit(Form)
+        self.lnx_monto.setGeometry(QtCore.QRect(420, 90, 181, 41))
+        self.lnx_monto.setObjectName("lnx_monto")
+        self.label_3 = QtWidgets.QLabel(Form)
+        self.label_3.setGeometry(QtCore.QRect(100, 180, 261, 41))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        self.lnx_cuotas = QtWidgets.QComboBox(Form)
+        self.lnx_cuotas.setGeometry(QtCore.QRect(420, 170, 181, 41))
+        self.lnx_cuotas.setObjectName("lnx_cuotas")
+        self.lnx_cuotas.addItem("")
+        self.lnx_cuotas.addItem("")
+        self.lnx_cuotas.addItem("")
+        self.lnx_cuotas.addItem("")
+        self.label_4 = QtWidgets.QLabel(Form)
+        self.label_4.setGeometry(QtCore.QRect(100, 270, 291, 41))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
+        self.lnx_ingresos = QtWidgets.QLineEdit(Form)
+        self.lnx_ingresos.setGeometry(QtCore.QRect(420, 260, 181, 41))
+        self.lnx_ingresos.setObjectName("lnx_ingresos")
+        self.label_5 = QtWidgets.QLabel(Form)
+        self.label_5.setGeometry(QtCore.QRect(100, 350, 291, 41))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_5.setFont(font)
+        self.label_5.setObjectName("label_5")
+        self.btn_archivos = QtWidgets.QPushButton(Form)
+        self.btn_archivos.setGeometry(QtCore.QRect(420, 350, 181, 41))
+        self.btn_archivos.setObjectName("btn_archivos")
+        self.label_6 = QtWidgets.QLabel(Form)
+        self.label_6.setGeometry(QtCore.QRect(100, 420, 291, 41))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_6.setFont(font)
+        self.label_6.setObjectName("label_6")
+        self.lnx_garantia = QtWidgets.QLineEdit(Form)
+        self.lnx_garantia.setGeometry(QtCore.QRect(420, 430, 181, 41))
+        self.lnx_garantia.setObjectName("lnx_garantia")
+        self.btn_solicitar = QtWidgets.QPushButton(Form)
+        self.btn_solicitar.setGeometry(QtCore.QRect(260, 500, 201, 61))
+        self.btn_solicitar.setObjectName("btn_solicitar")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+        self.btn_solicitar.clicked.connect(self.guardar)
+        self.btn_archivos.clicked.connect(self.pdf_abrir)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.label_2.setText(_translate("Form", "<html><head/><body><p><span style=\" color:#ffffff;\""
+                                                ">Monto solicitado: </span></p></body></html>"))
+        self.lnx_monto.setPlaceholderText(_translate("Form", "Monto"))
+        self.label_3.setText(_translate("Form", "<html><head/><body><p><span style=\" color:#ffffff;\""
+                                                ">Numero de cuotas:</span></p><p><br/></p></body></html>"))
+        self.lnx_cuotas.setItemText(0, _translate("Form", "3 meses"))
+        self.lnx_cuotas.setItemText(1, _translate("Form", "6 meses"))
+        self.lnx_cuotas.setItemText(2, _translate("Form", "12 meses"))
+        self.lnx_cuotas.setItemText(3, _translate("Form", "24 meses"))
+        self.label_4.setText(_translate("Form", "<html><head/><body><p><span style=\" color:#ffffff;\""
+                                                ">Ingresos mensuales:</span></p><p><br/></p></body></html>"))
+        self.lnx_ingresos.setPlaceholderText(_translate("Form", "Ingresos"))
+        self.label_5.setText(_translate("Form", "<html><head/><body><p><span style=\" color:#ffffff;\""
+                                                ">Archivos adjuntos:</span></p><p><br/></p></body></html>"))
+        self.btn_archivos.setText(_translate("Form", "Ingrese pdf"))
+        self.label_6.setText(_translate("Form", "<html><head/><body><p><span style=\" color:#ffffff;\""
+                                                ">Garantias:</span></p></body></html>"))
+        self.lnx_garantia.setPlaceholderText(_translate("Form", "Garantia"))
+        self.btn_solicitar.setText(_translate("Form", "Solicitar Prestamo"))
+
+    def guardar(self):
+
+
+        self.monto = self.lnx_monto.text()
+        self.cuotas = self.lnx_cuotas.currentText()
+        self.ingresos = self.lnx_ingresos.text()
+        self.archivos = None
+        self.garantia = self.lnx_garantia.text()
+
+    def pdf_abrir(self):
+        path = QtWidgets.QFileDialog.getOpenFileName()[0]
+        self.archivos.setText(path)
